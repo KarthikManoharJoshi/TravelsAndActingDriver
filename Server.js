224b436a8b50e04a3a14b218737d0c6b06d1cb86
Karthik Manohar <karthikManoharjoshi.1604064@Srec.ac.in>
2025-10-18 10:57:40 +0530

    Use Render dynamic port

--- Server.js
+++ Server.js
@@ -133,8 +133,15 @@ const admins = {
 //app.listen(3000, '0.0.0.0', () => {
 //  console.log('Server running on port 3000');
 //});
-const PORT = 3000;
+//const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+
+//app.listen(PORT, () => {
+//  console.log(`Server running on port ${PORT}`);
+//});
